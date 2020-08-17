e3cdfa5ad312f86276b0fb446094aa0b63e076da
Deeptendu Santra <santradibbo@gmail.com>
2020-08-17 21:14:16 +0000

    update

--- test_script.py
+++ test_script.py
@@ -1,19 +1,23 @@
 import urllib
 import datetime
 
-url = "https://docs.google.com/forms/d/e/1FAIpQLScf-ISET-XOiEBDyRtpuWIz4S98iC6Nzq247OgIAo4Dvs6vlA/" + "formResponse?"
+url = "https://docs.google.com/forms/d/e/1FAIpQLScf-ISET-XOiEBDyRtpuWIz4S98iC6Nzq247OgIAo4Dvs6vlA/" + "formResponse?&"
 
 email = "santradibbo@gmail.com"
 name = "Deeptendu Santra"
 section = "A"
-roll = 9
-date = datetime.date.today().strftime("%d-%m-%Y")
+roll = "9"
+date = datetime.date.today().isoformat()
 
-data= {"entry.375405198":name, "entry.1748177020" : section, "entry.1479767868" : roll,
+data1 = {"entry.375405198":name, "entry.1748177020" : section, 
+        "emailAddress" : email,
+        "entry.1479767868" : roll,
         "entry.1691501242" : date}
 
-response = urllib.parse.urlencode(data);
+data = {"emailAddress" : email,"entry.375405198":name,"entry.1748177020" : section,"entry.1479767868" : roll,"entry.1691501242" : date}
+
+response = urllib.parse.urlencode(data)
 
 form_response = url + response
 
-print(form_url)
+print(form_response)
